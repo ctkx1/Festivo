2bd83cebe13c5e8379c91dfef8ea56de33f526ca
ctk <czapa285@gmail.com>
2025-05-25 22:38:42 +0200

    update sekcji mapy

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,5 +1,5 @@
 import MapView, { Marker, Overlay } from "react-native-maps"
-import { Text, View } from "react-native"
+import { Text, View, Image } from "react-native"
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
 
 const Map = () => {
@@ -21,32 +21,82 @@ const Map = () => {
 					]}
 				/>
 				<Marker
-					coordinate={{ latitude: 51.4765, longitude: 18.3511 }}
-					title='Scena 1'
-				/>
+					coordinate={{ latitude: 51.4767, longitude: 18.35114 }}
+					title='Scena BLUE'>
+					<Image
+    				source={require("../../assets/icons/stage1_pin.png")}
+    				style={{ width: 40, height: 40 }}
+    				resizeMode="contain"
+  					/>
+				</Marker>
 				<Marker
-					coordinate={{ latitude: 51.4763, longitude: 18.35253 }}
-					title='Scena 2'
-				/>
-				<Marker
-					coordinate={{ latitude: 51.4776, longitude: 18.352 }}
-					title='Scena 3'
-				/>
+					coordinate={{ latitude: 51.4765, longitude: 18.35256 }}
+					title='Scena GREEN'>
+					<Image
+    				source={require("../../assets/icons/stage2_pin.png")}
+    				style={{ width: 40, height: 40 }}
+    				resizeMode="contain"
+  					/>				
+				</Marker>
 
 				<Marker
-					coordinate={{ latitude: 51.477, longitude: 18.3509 }}
-					title='Strefa namiotowa'
-				/>
+					coordinate={{ latitude: 51.47775, longitude: 18.35203 }}
+					title='Scena RED'>
+					<Image
+    				source={require("../../assets/icons/stage3_pin.png")}
+    				style={{ width: 40, height: 40 }}
+    				resizeMode="contain"
+  					/>
+
+				</Marker>
 
 				<Marker
-					coordinate={{ latitude: 51.477, longitude: 18.3522 }}
-					title='Sprite Zone'
-				/>
+					coordinate={{ latitude: 51.47757, longitude: 18.35256 }}
+					title='Scena POWER'>
+					<Image
+    				source={require("../../assets/icons/stage_power.png")}
+    				style={{ width: 40, height: 40 }}
+    				resizeMode="contain"
+  					/>
+
+				</Marker>
+
+				<Marker coordinate={{ latitude: 51.477, longitude: 18.3509 }}
+				title='Strefa namiotowa'>
+					<Image
+    				source={require("../../assets/icons/tent_pin.png")}
+    				style={{ width: 40, height: 40 }}
+    				resizeMode="contain"
+  					/>
+				</Marker>
+				
+
+				<Marker coordinate={{ latitude: 51.47715, longitude: 18.35219 }} 	title="Sprite Zone">
+  					<Image
+    				source={require("../../assets/icons/sprite_pin.png")}
+    				style={{ width: 40, height: 40 }}
+    				resizeMode="contain"
+  					/>
+				</Marker>
 
 				<Marker
-					coordinate={{ latitude: 51.477, longitude: 18.3539 }}
-					title='Łazienki'
-				/>
+					coordinate={{ latitude: 51.47725, longitude: 18.3536 }}
+					title='Łazienki'>
+					<Image
+    				source={require("../../assets/icons/WC_pin.png")}
+    				style={{ width: 40, height: 40 }}
+    				resizeMode="contain"
+  					/>
+				</Marker>
+				<Marker
+					coordinate={{ latitude: 51.4759, longitude: 18.35123 }}
+					title='Bramki Festiwalu'>
+					<Image
+    				source={require("../../assets/icons/pin_gate_entry.png")}
+    				style={{ width: 40, height: 40 }}
+    				resizeMode="contain"
+  					/>
+				</Marker>
 			</MapView>
 		</View>
 	)
